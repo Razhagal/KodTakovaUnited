37fc3bc490cc987e8778f9d00282664649637792
h.petrov <h.petrov@xs-software.com>
2020-02-01 00:56:46 +0200

    added player, added pixel perfect camera package, updated player test scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed = 1;
+
+    private Rigidbody2D rBody;
+    private Vector2 inputVector;
+    private Vector2 movement;
+    private Vector2 newPos;
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+
+        movement = inputVector * MoveSpeed;
+
+        Vector2 currPos = rBody.position;
+        newPos = currPos + movement * Time.fixedDeltaTime;
+
+
+        rBody.MovePosition(newPos);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+}
