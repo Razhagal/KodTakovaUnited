53808b7d38240c70f9297685ea1dc691517028ff
h.petrov <h.petrov@xs-software.com>
2020-02-01 01:09:05 +0200

    Added Lootable tag and LootableItem class, updated player

---
+++ Assets/Scripts/LootableItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LootableItem : MonoBehaviour
+{
+    public enum LootType
+    {
+        ShipPart,
+        PlayerPart,
+        EnergyItem
+    }
+
+    public LootType ItemType;
+}
