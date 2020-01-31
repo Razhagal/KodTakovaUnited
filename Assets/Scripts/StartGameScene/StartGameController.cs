962392a4b0b01e9f16bda88490328ee8714bacd4
AleksandarTanev <aleksander.tanev.51@gmail.com>
2020-02-01 00:28:53 +0200

    add textmeshPro, add Start Game Scene

---
+++ Assets/Scripts/StartGameScene/StartGameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameController : MonoBehaviour
+{
+    public void OnClickStartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
