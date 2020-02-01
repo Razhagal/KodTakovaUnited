1310103c4d92597fd001e633c6e8ec2f615c5702
Katerina Koceva <k.kotseva@xs-software.com>
2020-02-01 17:38:57 +0200

    Map and Robot - update

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-    public Transform[] spaceshipParts;
+    public GameObject[] spaceshipParts;
     public Transform[] robotParts;
     public Transform playerStartPosition;
-   
 
+    
     public void ResetMap()
     {
 
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        
     }
 }
