b26e8629f5e9eb672778f12cd6d687f6e9a6197c
Katerina Koceva <k.kotseva@xs-software.com>
2020-02-01 05:13:42 +0200

    Map-wip

---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public Transform[] spaceshipParts;
+    public Transform[] robotParts;
+    public Transform playerStartPosition;
+   
+
+    public void ResetMap()
+    {
+
+    }
+}
